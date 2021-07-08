5a5a1dbd34a76c3cdf780c660977a690c7c67ff0
potatomasterrace <reda.bourial@gmail.com>
2021-07-08 12:10:50 +0200

    use store inapp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux'
 import CommunicationZone from './CommunicationZone';
 
 function App() {
+  const conversations = useSelector((state) => state.app.conversations)
+  const dispatch = useDispatch();
   return (
     <div className="App">
-    <div className="mainContainer">
-        <CommunicationZone/>
-    </div>
+      <div className="conversationListContainer">
+        {conversations.map(
+          (v, index) => (
+            <div>
+              <button onClick={() =>{
+                console.log(`switched to conversation`,index)
+                dispatch({
+                type: 'app/switchToConversation',
+                payload: {
+                  index
+                }
+              })}}
+              >
+                {v}
+              </button>
+            </div>
+          )
+        )}
+      </div>
+      <div className="mainContainer">
+        <CommunicationZone />
+      </div>
     </div>
   );
 }
