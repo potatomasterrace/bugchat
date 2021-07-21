6279eae8a4b487420d6f8801c7b62986b51b4230
potatomasterrace <reda.bourial@gmail.com>
2021-07-21 07:47:50 +0200

    ui improvement

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux'
 import CommunicationZone from './components/CommunicationZone';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 function App() {
   const loading = useSelector((state) => state.app.loading)
@@ -25,33 +27,42 @@ function App() {
   if (loading) {
     return '';
   }
-
+  const getSizes = (size)=>(['xs', 'sm','md','lg','xl'].reduce((acc,v)=>{
+    acc[v] = size;
+    return acc;
+  },{}) )
   // PIMP THIS PART
   return (
     <div className="App">
-      <div className="conversationListContainer">
-        {friends.map(
-          (v, index) => (
-            <div>
-              <button onClick={() => {
-                console.log(`switched to conversation`, index)
-                dispatch({
-                  type: 'app/switchToConversation',
-                  payload: {
-                    index
-                  }
-                })
-              }}
-              >
-                {v.name}
-              </button>
-            </div>
-          )
-        )}
-      </div>
-      <div className="mainContainer">
-        <CommunicationZone currentDuckIndex={currentDuckIndex} />
-      </div>
+      <Grid className="grid" container spacing={0}>
+        <Grid item {...getSizes(1)}>
+          <div className="conversationListContainer">
+            {friends.map(
+              (v, index) => (
+                <div>
+                  <button onClick={() => {
+                    console.log(`switched to conversation`, index)
+                    dispatch({
+                      type: 'app/switchToConversation',
+                      payload: {
+                        index
+                      }
+                    })
+                  }}
+                  >
+                    {v.name}
+                  </button>
+                </div>
+              )
+            )}
+          </div>
+        </Grid>
+        <Grid item {...getSizes(11)}>
+          <div className="mainContainer">
+            <CommunicationZone currentDuckIndex={currentDuckIndex} />
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
