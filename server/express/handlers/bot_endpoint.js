b754e8b3c5f5334b9ee7a55b941b76b4f8f696d3
potatomasterrace <reda.bourial@gmail.com>
2021-07-21 05:03:34 +0200

    change bot response to dbVersion

--- server/express/handlers/bot_endpoint.js
+++ server/express/handlers/bot_endpoint.js
@@ -1,13 +1,12 @@
 const bots = require('./bots');
+const db = require('./utils/database');
 
 module.exports = function (req, res) {
     try {
         const idx = parseInt(req.params['idx']);
         const {message} = req.body;
         bots[idx].dialogueEngine(message)
-        res.json({
-            ok: true,
-        });
+        res.json({dbVersion: db.getVersion()});
     } catch (e) {
         res.json({
             error: String(e),
